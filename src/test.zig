db1c62a04897f449b0b557b957e03755794e5752
Brett Jurman <i.be.brett@gmail.com>
2018-11-25 21:58:17 -0500

    parametrizing the allocator by size of buffer

--- src/test.zig
+++ src/test.zig
@@ -4,17 +4,16 @@ const std = @import("std");
 const debug = std.debug;
 const Allocator = std.mem.Allocator;
 
-
 test "test idiotic alloc low value" {
-  var allocator: Allocator = zalloc.idiotic_allocator;
-  var slice = try allocator.alloc(*i32, 100);
+  var ia = zalloc.IdioticAllocator(200).init();
+  var slice = try ia.allocator.alloc(u8, 100);
 
   debug.assert(slice.len == 100);
 }
 
 test "test alloc too big" {
-  var allocator: Allocator = zalloc.idiotic_allocator;
-  if (zalloc.idiotic_allocator.alloc(*i32, 10000)) |slice| {
+  var ia = zalloc.IdioticAllocator(200).init();
+  if (ia.allocator.alloc(u8, 201)) |slice| {
     @panic("Should not be able to reserve an array of 10000");
   } else |err| {
     debug.assert(err == Allocator.Error.OutOfMemory);
--- src/zalloc/index.zig
+++ src/zalloc/index.zig
@@ -2,44 +2,43 @@ const std = @import("std");
 
 const Allocator = std.mem.Allocator;
 
-const bufferSize = 2048;
-var buffer = []u8{0} ** bufferSize;
-
-/// A "IdioticAllocator." An idiotic allocator that does not free, ignores
+/// An "IdioticAllocator." An idiotic allocator that does not free, ignores
 /// alignment and simply returns an offset into a static buffer. Not useful
 /// for anything but a learning tool.
-pub const IdioticAllocator = struct {
-  index: usize,
-  pub allocator: Allocator,
+pub fn IdioticAllocator(comptime bufferSize: usize) type {
+  return struct {
+    const Self = @This();
+    buffer: [bufferSize]u8,
+    index: usize,
+    pub allocator: Allocator,
 
-  fn allocFn(allocator: *Allocator, n: usize, alignment: u29) ![]u8 {
-    const self = @fieldParentPtr(IdioticAllocator, "allocator", allocator);
+    fn allocFn(allocator: *Allocator, n: usize, alignment: u29) ![]u8 {
+      const self = @fieldParentPtr(Self, "allocator", allocator);
+      if (n + self.index > self.buffer.len) {
+        return Allocator.Error.OutOfMemory;
+      }
 
-    if (n > bufferSize) {
-      return Allocator.Error.OutOfMemory;
+      var ret = self.buffer[self.index..n+self.index];
+      self.index += n;
+      return ret;
     }
 
-    var ret = buffer[self.index..n+self.index];
-    self.index += n;
-    return ret;
-  }
+    fn reallocFn(self: *Allocator, old_mem: []u8, new_size: usize, alignment: u29) ![]u8 {
+      return self.allocFn(self, new_size, alignment);
+    }
 
-  fn reallocFn(self: *Allocator, old_mem: []u8, new_size: usize, alignment: u29) ![]u8 {
-    return self.allocFn(self, new_size, alignment);
-  }
+    fn freeFn(self: *Allocator, old_mem: []u8) void {}
 
-  fn freeFn(self: *Allocator, old_mem: []u8) void {}
-
-  pub fn init() IdioticAllocator {
-    return IdioticAllocator {
-      .allocator = Allocator {
-        .allocFn = allocFn,
-        .reallocFn = reallocFn,
-        .freeFn = freeFn
-      },
-      .index = 0
-    };
-  }
-};
-
-pub var idiotic_allocator = IdioticAllocator.init().allocator;
+    pub fn init() Self {
+      return Self {
+        .allocator = Allocator {
+          .allocFn = allocFn,
+          .reallocFn = reallocFn,
+          .freeFn = freeFn
+        },
+        .buffer = []u8{0} ** bufferSize,
+        .index = 0
+      };
+    }
+  };
+}
